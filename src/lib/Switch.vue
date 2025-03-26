4722d8262322185182d42aecf6391b3fa4a8ece6
chair <526248620@qq.com>
2025-03-26 17:05:31 +0800

    实现switch点击开关

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,9 +1,14 @@
 <template>
-    <button><span></span></button>
+    <button :class="{ checked }" @click="toggle"><span></span></button>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const checked = ref(false);
+const toggle = () => {
+    checked.value = !checked.value;
+};
 </script>
 
 <style scoped lang="scss">
@@ -23,10 +28,13 @@ span {
     position: absolute;
     top: 2px;
     left: 2px;
-    // left: calc(100% - #{$h2} - 2px);
     height: $h2;
     width: $h2;
     background-color: white;
     border-radius: calc($h2 / 2);
+}
+
+.checked>span {
+    left:calc(100% - #{$h2} - 2px);
 }
 </style>
