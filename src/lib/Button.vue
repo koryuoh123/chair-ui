d1cf860f96249c610626dde8a830bad0d09e7b4e
chair <526248620@qq.com>
2025-03-26 20:02:57 +0800

    使属性绑定能响应式变化

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="btncontainer">
-        <button v-bind="$attrs">
+    <div class="btncontainer" :size="size">
+        <button v-bind="rest">
             <slot></slot>
         </button>
     </div>
@@ -15,10 +15,31 @@
 defineOptions({
     inheritAttrs: false
 })
+/**
+ * 通过useAttrs获取父组件传递的属性
+ * 通过解构获取size属性，然后通过rest获取剩余属性
+ */
+import { useAttrs, ref, computed } from 'vue';
+/**
+ * 为开发者优化，使属性可以响应式变化
+ */
+const attrs = useAttrs();
+const size = computed(() => attrs.size); // 响应式获取 size
+const rest = computed(() => {
+    const { size, ...otherAttrs } = attrs;
+    return otherAttrs;
+});
+
 </script>
 
 <style scoped lang="scss">
 .btncontainer {
     border: 1px solid red;
+
+    &[size="large"] {
+        button {
+            font-size: 20px;
+        }
+    }
 }
 </style>
